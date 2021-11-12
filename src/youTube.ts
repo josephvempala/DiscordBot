0739948467420283e10d6f49d65ea15ff99908b7
joseph <josephvempala@gmail.com>
2021-11-12 07:10:47 +0530

    handled 410 error in getBasicInfo in parseYtPlayParam

--- src/youTube.ts
+++ src/youTube.ts
@@ -22,7 +22,7 @@ export async function getYoutubeAudioStream(url : string) : Promise<GetAudioStre
             return null;
         const stream = downloadFromInfo(videoInfo!, {filter:"audioonly", highWaterMark: 1<<25 })
             .on("error",(e : any)=>{
-                if(e.statusCode! === 403||401){
+                if(e.statusCode! === 403||410){
                     return null;
                 }
                 console.log(e);
@@ -37,24 +37,30 @@ export async function getYoutubeAudioStream(url : string) : Promise<GetAudioStre
 
 export async function parseYouTubePlayParameter(param : string) : Promise<basicVideoInfo[] | null>{
     const result : basicVideoInfo[] = [];
-    const playlistID = await ytpl.getPlaylistID(param).catch(() => null);
-    if(playlistID){
-        const playlistQueryResult = await ytpl(playlistID);
-        playlistQueryResult.items.map(x => result.push({url : x.url, title : x.title, length: x.durationSec!}));
-        return result;
+    try{
+        const playlistID = await ytpl.getPlaylistID(param).catch(() => null);
+        if(playlistID){
+            const playlistQueryResult = await ytpl(playlistID);
+            playlistQueryResult.items.map(x => result.push({url : x.url, title : x.title, length: x.durationSec!}));
+            return result;
+        }
+        const basicVideoInfo = await getBasicInfo(param).catch(() => null);
+        if(basicVideoInfo) return [{url: param, title: basicVideoInfo.player_response.videoDetails.title, length : +basicVideoInfo.videoDetails.lengthSeconds}];
+        const searchStringResult = await ytsr.getFilters(param)
+            .then(x => x.get('Type')!.get('Video'))
+            .catch(() => null);
+        if(!searchStringResult?.url)
+            return null;
+        const finalLinks = await ytsr(searchStringResult.url, {limit:1}).catch(() => null);
+        if(finalLinks){
+            const link = finalLinks.items[0] as basicVideoInfo;
+            const basicVideoInfo = await getBasicInfo(link.url).catch(() => null);
+            return [{url:link.url, title : link.title, length:+basicVideoInfo!.videoDetails.lengthSeconds}]
+        }
     }
-    const basicVideoInfo = await getBasicInfo(param).catch(() => null);
-    if(basicVideoInfo) return [{url: param, title: basicVideoInfo.player_response.videoDetails.title, length : +basicVideoInfo.videoDetails.lengthSeconds}];
-    const searchStringResult = await ytsr.getFilters(param)
-        .then(x => x.get('Type')!.get('Video'))
-        .catch(() => null);
-    if(!searchStringResult?.url)
+    catch (e : any){
+        console.log(e);
         return null;
-    const finalLinks = await ytsr(searchStringResult.url, {limit:1}).catch(() => null);
-    if(finalLinks){
-        const link = finalLinks.items[0] as basicVideoInfo;
-        const basicVideoInfo = await getBasicInfo(link.url).catch(() => null);
-        return [{url:link.url, title : link.title, length:+basicVideoInfo!.videoDetails.lengthSeconds}]
     }
     return null;
 }
