dc2e443115eb78e74d0ca01567e1abecd6a06c91
joseph <josephvempala@gmail.com>
2021-11-13 21:31:48 +0530

    added previously uncommitted files

---
+++ dist/IBasicVideoInfo.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VideoInfoType = void 0;
+var VideoInfoType;
+(function (VideoInfoType) {
+    VideoInfoType[VideoInfoType["Spotify"] = 0] = "Spotify";
+    VideoInfoType[VideoInfoType["YouTube"] = 1] = "YouTube";
+    VideoInfoType[VideoInfoType["SoundCloud"] = 2] = "SoundCloud";
+})(VideoInfoType = exports.VideoInfoType || (exports.VideoInfoType = {}));
+//# sourceMappingURL=IBasicVideoInfo.js.map
