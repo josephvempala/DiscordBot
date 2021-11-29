8e43b9cb6f4d3835086021a4e042eedbfc9840ec
joseph <josephvempala@gmail.com>
2021-11-29 17:31:19 +0530

    added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const ForkTsCheckerPlugin = require('fork-ts-checker-webpack-plugin');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode : 'development',
+    target: 'node',
+    entry: './src/index.ts',
+    resolve:{
+        extensions: ['.ts','.js','.json'],
+        mainFields: ["main"],
+
+    },
+    stats: 'errors-only',
+    module:{
+        rules:[
+            {
+                test: /\.(js|ts)?$/,
+                loader: 'esbuild-loader',
+                options: {
+                    loader : 'tsx',
+                    target: 'es2018'
+                },
+                exclude : /node_modules/,
+            }
+        ]
+    },
+    plugins: [new NodemonPlugin(), new ForkTsCheckerPlugin()],
+    output: {
+        filename: 'app.js',
+        path : path.resolve(__dirname, 'dist')
+    }
+};
